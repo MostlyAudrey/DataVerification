a75d8e3315c6368dfc23d78ded232959738e67b3
Audrey <audreyyarmo@gmail.com>
2021-03-12 13:17:17 -0500

    Initial commit

---
+++ proof_of_concept/main.py
@@ -0,0 +1,67 @@
+#/usr/bin/python3
+
+Cities = [
+( 1, 'Baltimore', 'MD', 'US' ),
+( 2, 'Atlanta',   'GA', 'US' ),
+( 3, 'Dallas',    'TX', 'US' ),
+( 4, 'Demoins',   'IO', 'US' ),
+( 5, 'Paris',     'P',  'FR')
+]
+
+Client = [
+( 1, 'Joy Arulraj',             35, '1 North Ave',              2, ' 30332'),
+( 2, 'Bob Waters',              86, '123 Marietta St.',         4, ' 30341'),
+( 3, 'Bill Leahy',              69, 'A nice beach somewhere',   3, ' 30312'),
+( 4, 'Kishore Ramachandrian',   41, '2 Clough Lane',            2, ' 12423'),
+]
+
+Orders = [
+( 1, 1, '2021-02-10' ),
+( 2, 1, '2021-02-13' ),
+( 3, 2, '2021-02-18' ),
+( 4, 4, '2021-02-19' ),
+( 5, 1, '2021-02-29' ),
+( 6, 3, '2021-03-08' ),
+( 7, 2, '2021-03-12' ),
+]
+
+LineItems = [
+( 1,  1, 1,   15, 33.00 ),
+( 2,  1, 12,  2,  5.00  ),
+( 3,  1, 101, 20, 3.75  ),
+( 4,  2, 1,   15, 33.00 ),
+( 5,  2, 12,  2,  5.00  ),
+( 6,  2, 101, 20, 3.75  ),
+( 7,  2, 1,   15, 33.00 ),
+( 8,  3, 12,  2,  5.00  ),
+( 9,  3, 101, 20, 3.75  ),
+( 10, 4, 1,   15, 33.00 ),
+( 11, 5, 12,  2,  5.00  ),
+( 12, 5, 101, 20, 3.75  ),
+( 13, 5, 1,   15, 33.00 ),
+( 14, 5, 12,  2,  5.00  ),
+( 15, 6, 101, 20, 3.75  ),
+( 16, 6, 1,   15, 33.00 ),
+( 17, 6, 12,  2,  5.00  ),
+( 18, 7, 101, 20, 3.75  ),
+]
+
+Products = [
+( 1,   'Garden Hose',         'Plant inc.'       ),
+( 2,   'Garden Hose',         'Plant inc.'       ),
+( 12,  'Strawberry Poptarts', 'Kellogs'         ),
+( 13,  'BlueBerry Poptarts',  'Kellogs'          ),
+( 14,  'Strawberry Poptarts', 'Kellogs'          ),
+( 15,  'Strawberry Poptarts', 'Kellogs'          ),
+( 16,  'Strawberry Poptarts', 'Kellogs'          ),
+( 17,  'Strawberry Poptarts', 'Kellogs'          ),
+( 18,  'Strawberry Poptarts', 'Kellogs'          ),
+( 19,  'Strawberry Poptarts', 'Kellogs'          ),
+( 101, 'Honey Nut Cherios',   'General Mills'    ),
+( 102, 'Honey Nut Cherios',   'General Mills'    ),
+( 103, 'Honey Nut Cherios',   'General Mills'    ),
+( 104, 'Honey Nut Cherios',   'General Mills'    ),
+( 105, 'Honey Nut Cherios',   'General Mills'    ),
+( 106, 'Honey Nut Cherios',   'General Mills'    ),
+( 107, 'Honey Nut Cherios',   'General Mills'    ),
+]
---
+++ proof_of_concept/node.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+
+class MerkleNode:
+
+	def __init__(self, left_child, right_child)
+		self.left_child = left_child
+		self.right_child = right_child
