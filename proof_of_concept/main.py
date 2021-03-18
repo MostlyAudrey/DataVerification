136ec2af2491f31f8d44c0443ee90af203334897
Jermiah Russell <jermiah83@gmail.com>
2021-03-18 18:29:25 -0400

    Main can now traverse the relation and construct the full MTree

--- proof_of_concept/main.py
+++ proof_of_concept/main.py
@@ -1,7 +1,7 @@
 #/usr/bin/python3
 
 import hashlib
-import node
+from node import LeafNode, InnerNode
 
 
 Cities = [
@@ -72,15 +72,74 @@ Products = [
 
 
 
-def CreateOrder(order_id, line_item_ids):
+# def CreateOrder(order_id, line_item_ids):
 
-    client_id = Orders[order_id][1]
+#     client_id = Orders[order_id - 1][1]
 
-    client_node = node.LeafNode('tb_client', client_id, hashlib.sha256( str(Clients[client_id]).encode() ) )
-    order_node  = node.LeafNode('tb_order',  order_id,  hashlib.sha256( str(Orders[order_id]).encode() ) )
-    print(client_node)
-    print(order_node)
+#     client_node = node.LeafNode('tb_client', client_id, hashlib.sha256( str(Clients[client_id]).encode() ) )
+#     order_node  = node.LeafNode('tb_order',  order_id,  hashlib.sha256( str(Orders[order_id]).encode() ) )
+#     print(client_node)
+#     print(order_node)
 
+# CreateOrder(1, [1,2,3])
 
+def CreateOrder(order_id):
+    order = Orders[order_id - 1]
+    client = Clients[order[1] - 1]
+    city = Cities[client[4] - 1]
+    line_items = [item for item in LineItems if item[1] == order_id]
+    product_ids = [item[2] for item in line_items]
+    products = [p for p in Products if p[0] in product_ids]
 
-CreateOrder(1, [1,2,3])
+    leaf_data = [order, client, city] + line_items + products
+
+    leaf_nodes = []
+    for data in leaf_data:
+    	# print(data)
+    	table = ""
+    	if data in Cities:
+    		table = 'tb_city'
+    	elif data in Clients:
+    		table = 'tb_client'
+    	elif data in Orders:
+    		table = 'tb_order'
+    	elif data in LineItems:
+    		table = 'tb_lineitem'
+    	elif data in Products:
+    		table = 'tb_product'
+
+    	node = LeafNode(table, data[0], hashlib.sha256(str(data).encode()))
+    	leaf_nodes.append(node)
+    	# print(node)
+    return leaf_nodes
+
+treeNodes = []
+root_node = None
+def CreateTree(leaf_nodes):
+	global treeNodes, root_node
+	
+	if len(leaf_nodes) == 1:
+		root_node = leaf_nodes[0]
+		treeNodes.append(root_node)
+		return
+
+	parent_nodes = []
+	i = 0
+	while i < len(leaf_nodes):
+		lchild = leaf_nodes[i]
+		rchild = None
+		if (i + 1) < len(leaf_nodes):
+			rchild = leaf_nodes[i + 1]
+
+		pnode = InnerNode(lchild, hashlib.sha256(str(lchild).encode()), rchild, hashlib.sha256(str(rchild).encode()))
+		print(pnode)
+		parent_nodes.append(pnode)
+		i += 2
+
+	treeNodes += parent_nodes
+	CreateTree(parent_nodes)
+
+leaf_nodes = CreateOrder(1)
+print(leaf_nodes)
+CreateTree(leaf_nodes)
+print(treeNodes)
